55c7da3bee77459f0716bc71aed49c6b861c6383
anom35 <freelance35830@gmail.com>
2023-02-21 19:25:31 +0100

    test creation module fiche logement

---
+++ src/components/Fiche-logement.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function ficheLogement() {
+  return (
+    <div className='fiche-logement'>Fiche-logement</div>
+  )
+}
+
+export default ficheLogement
--- src/components/Logements.jsx
+++ src/components/Logements.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import ficheLogement from '../components/Fiche-logement'
+import "../styles/Logements.css"
+import "../styles/Fiche-logement.css"
 
 function Logements() {
   return (
-    <div>Logements</div>
+    <div className='logements'>
+        <ficheLogement />
+    </div>
   )
 }
 
