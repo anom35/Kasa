a78e3b300fb6e0aaa723757dc5c296e8872a34ef
anom35 <freelance35830@gmail.com>
2023-02-25 17:34:56 +0100

    Fonction Five stars

--- src/components/Logement.jsx
+++ src/components/Logement.jsx
@@ -2,20 +2,49 @@ import React, { useState } from 'react'
 import { useSearchParams } from 'react-router-dom';
 import Dropdown from 'react-dropdown';
 import Navbar from "./Navbar"
-import Banner from './Banner';
+import Footer from "./Footer"
+import Stars from "./Stars"
+import "../styles/Navbar.css"
 import "../styles/Logement.css"
+import records from "../datas/logements.json"
 
 function Logement() {
     const [searchParams] = useSearchParams();
     const [query] = useState(searchParams.get('_id'));
+    const record = records.find(element => element.id === query)
     
-    return (
-        <div className='logement'>
-            <Navbar />
-            <Banner />
-            <Dropdown />
-        </div>
-    )
+    let cpt = 0;
+    if (record !== undefined) {
+        return (
+            <div>
+                <div className='logement'>
+                    <Navbar />
+                    <img src={record.cover} alt={record.title} />
+                    <div className='ficheLogement'>
+                        <div className='div-description'>
+                            <h1>{record.title}</h1>
+                            <h4>{record.location}</h4>
+                            <div className='div-tags'>
+                                {
+                                    record.tags.map(element => {
+                                        cpt += 1;
+                                        return(<p className='tags' key={cpt}>{element}</p>)
+                                    })
+                                }
+                            </div>
+                        </div>
+                        <div className='div-etoiles'>
+                                <p>{record.host.name}</p>
+                                <img src={record.host.picture} alt={record.title} />
+                        </div>
+                    </div>
+                    <Stars rating={record.rating} />
+                    <Dropdown options={record.description}>Description</Dropdown>
+                </div>
+                <Footer />
+            </div>
+        )
+    }
 }
 
 export default Logement
